914922ffd33dc7f1f26ba174a71cabca3c2b6b30
Jan Manaloto <janmanaloto@gmail.com>
2025-02-02 16:03:17 -0500

    initialize project

---
+++ src/lib/components/Grid.svelte
@@ -0,0 +1,12 @@
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 1rem;
+    padding: 1rem;
+  }
+</style>
+
+<div class="grid">
+  <slot />
+</div>
---
+++ title=src/main.js
@@ -0,0 +1,7 @@
+import App from './App.svelte';
+
+const app = App({
+  target: document.body,
+});
+
+export default app; 
